6d86db64f3e29ce3d0250323824b9609c4cfc591
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-10 06:04:28 -0300

    Good Solution

---
+++ Generics2CSharp/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Generics2CSharp.Entities
+{
+    internal class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + " Price : $" + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentNullException("This Product is null on CompareTO");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
--- Generics2CSharp/Program.cs
+++ Generics2CSharp/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Generics2CSharp.Entities;
+using Generics2CSharp.Services;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
+
 
 namespace Generics2CSharp
 {
@@ -9,6 +11,23 @@ namespace Generics2CSharp
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>();
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string [] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                products.Add(new Product(name, price));
+
+            }
+            CalculationService cs = new CalculationService();
+
+            Console.Write("Max : ");
+            Product p = cs.Max(products); // <Product> is optional
+            Console.Write(p);
+            Console.ReadKey();
         }
     }
 }
